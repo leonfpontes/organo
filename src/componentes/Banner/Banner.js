b49aab0a45a9d32e108e7697de066bd176f07fcb
leonardo.pontes <leonardo.pontes@mag-it.com.br>
2024-04-21 10:57:51 -0300

    english names

--- src/componentes/Banner/Banner.js
+++ src/componentes/Banner/Banner.js
@@ -3,7 +3,7 @@ import "./Banner.css"
 export const Banner = () => {
     return (
         <header class="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+            <img src="/images/banner.png" alt="O banner principal da página do Organo"/>
         </header>
     )
 }
